40deb0d6339bed0ec4693a1074f19ac5c0d97156
alexUd01 <alexanderikpeama@gmail.com>
2023-08-07 23:32:08 +0100

    task 5

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,6 +9,11 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Doc here """
+        if path is None or excluded_paths is None:
+            return True
+        if path.rstrip('/') not in list(map(lambda x: x.rstrip('/'),
+                                            excluded_paths)):
+            return True
         return False
 
     def authorization_header(self, request=None) -> str:
