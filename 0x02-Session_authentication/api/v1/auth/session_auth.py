be25b5561b9656ca6a32083a56216e8cef8b700c
alexUd01 <alexanderikpeama@gmail.com>
2023-08-09 23:25:36 +0100

    task 5 and 6

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -16,7 +16,7 @@ class SessionAuth(Auth):
     def create_session(self, user_id: str = None) -> str:
         """ A method that creates a session id for a user_id """
         if user_id and type(user_id) is str:
-            session_id = uuid4()
+            session_id = str(uuid4())
             self.user_id_by_session_id[session_id] = user_id
             return session_id
         return None
@@ -25,4 +25,16 @@ class SessionAuth(Auth):
         """ A method that returns user id based on a session id """
         if session_id and type(session_id) is str:
             return self.user_id_by_session_id.get(session_id)
+        return None
+
+    def current_user(self, request=None):
+        """ A method that overloads and returns a user instance based on a
+        cookie value """
+        if request:
+            session_id = self.session_cookie(request)
+            if session_id:
+                user_id = self.user_id_for_session_id(session_id)
+                if user_id:
+                    user = User.get(user_id)
+                    return user
         return None
