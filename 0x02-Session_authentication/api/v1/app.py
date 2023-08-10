abc2df73211b1249d934a22e3a3dc8a32adda79d
alexUd01 <alexanderikpeama@gmail.com>
2023-08-10 19:13:16 +0100

    task 10

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -76,4 +76,4 @@ def b4_each_request() -> None:
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port,debug=True)
+    app.run(host=host, port=port)
