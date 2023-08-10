609a5602df56a4b6d132137aecfb0457c4d97c45
alexUd01 <alexanderikpeama@gmail.com>
2023-08-10 19:45:48 +0100

    task 10

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -10,23 +10,3 @@ class UserSession(Base):
         super().__init__(*args, **kwargs)
         self.user_id = kwargs.get('user_id')
         self.session_id = kwargs.get('session_id')
-
-    @property
-    def user_id(self):
-        """ `user_id` getter method """
-        return self.user_id
-
-    @user_id.setter
-    def user_id(self, user_id):
-        """ `use_id` setter method """
-        self.user_id = user_id
-
-    @property
-    def session_id(self):
-        """ `session_id` getter method """
-        return self.session_id
-
-    @session_id.setter
-    def session_id(self, session_id):
-        """ `session_id` getter method """
-        self.session_id = session_id
