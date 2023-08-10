c14ce1c9288efe71009e784bfcde2240fb8aba58
alexUd01 <alexanderikpeama@gmail.com>
2023-08-10 21:26:59 +0100

    task 10

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -18,7 +18,9 @@ class SessionDBAuth(SessionExpAuth):
         if user_id:
             session_id = super().create_session(user_id)
             if session_id:
-                user_session = UserSession(user_id, session_id)
+                user_session = UserSession(
+                    user_id=user_id, session_id=session_id
+                )
                 user_session.save()
                 return session_id
         return None
@@ -28,15 +30,18 @@ class SessionDBAuth(SessionExpAuth):
         A method that overloads `api.v1.auth.auth.SessionExpAuth` and
         returns a user id from a database based on `session_id`
         """
-        UserSession.load_from_file()
-        from models.base import DATA
+        if session_id:
+            UserSession.load_from_file()
+            from models.base import DATA
 
-        for item in DATA['UserSession'].values():
-            key = item.session_id
-            val = item.user_id
-            self.user_id_by_session_id[key] = val
-        user_id = super().user_id_for_session_id(session_id)
-        return user_id  # NOTE: Will return None sometimes
+            for item in DATA['UserSession'].values():
+                self.user_id_by_session_id[item.session_id] = {
+                    'user_id': item.user_id,
+                    'created_at': item.created_at
+                }
+            user_id = super().user_id_for_session_id(session_id)
+            return user_id  # NOTE: Will return None sometimes
+        return None
 
     def destroy_session(self, request=None):
         """
@@ -44,15 +49,17 @@ class SessionDBAuth(SessionExpAuth):
         destroys the `UserSession` based on the session id from the
         request cookie.
         """
-        # Remove session ID from session dict
-        destroyed = super().destroy_session(request)
-
-        # Remove session ID from global DATA dict
-        from models.base import DATA
-        session_id = self.session_cookie(request)
-        for k, obj in DATA['UserSession'].items():
-            if obj.session_id == session_id:
-                del DATA['UserSession'][k]
-                # Save changes
-                UserSession.save_to_file()
-        return destroyed
+        if request:
+            # Remove session ID from session dict
+            destroyed = super().destroy_session(request)
+            # Remove session ID from global DATA dict
+            from models.base import DATA
+            session_id = self.session_cookie(request)
+            if session_id:
+                for k, obj in DATA['UserSession'].items():
+                    if obj.session_id == session_id:
+                        del DATA['UserSession'][k]
+                        # Save changes
+                        UserSession.save_to_file()
+            return destroyed
+        return None
